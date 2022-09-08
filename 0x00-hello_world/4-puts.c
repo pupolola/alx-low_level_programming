7f82c3b48202592534cc337271f3087413a38497
Pupolola <pupololaabdul@gmail.com>
2022-09-08 16:41:56 -0700

    Hello,puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -8,7 +8,5 @@
 * Return: 0
 */
 int main(void)
-{
-       puts("\"Programming is like building a multilingual puzzle");
-       return (0);
-}
+{puts("\"Programming is like building a multilingual puzzle");
+return (0);}
