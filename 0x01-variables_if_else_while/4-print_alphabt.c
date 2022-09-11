c53532de0be2c1afbd267fa30b80b541bf48dc41
Pupolola <pupololaabdul@gmail.com>
2022-09-11 07:32:46 -0700

    print alphabt

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -8,8 +8,8 @@ int main(void)
 { char a;
 
 for (a = 'a'; a <= 'z'; a++)
-{ if (a == 'q' || a == 'e') }
+{ if (a == 'q' || a == 'e')
 { continue; }
-{ putchar(a); }
+putchar(a); }
 putchar('\n');
 return (0); }
