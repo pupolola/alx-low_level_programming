749237569df86f167f5f96f452e6c73a0eb20f23
Pupolola <pupololaabdul@gmail.com>
2022-09-12 13:34:45 -0700

    print comb4

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -10,9 +10,12 @@ int j;
 int k;
 
 for (i = 0; i <= 9; i++)
-{ for (j = 0; j <= 9; j++)
-{ for (k = 0; k <= 9; k++)
-{ if (k > j && j > i)
+{ 
+for (j = 0; j <= 9; j++)
+{ 
+for (k = 0; k <= 9; k++)
+{ 
+if (k > j && j > i)
 { putchar(i + '0');
 putchar(j + '0');
 putchar(k + '0');
