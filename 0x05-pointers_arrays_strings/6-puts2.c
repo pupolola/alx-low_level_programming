9caf993ab7575dc4befd8c9051faae29dbb472a0
Pupolola <pupololaabdul@gmail.com>
2022-09-19 09:48:58 -0700

    puts2

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+* puts2 - prints xters of a string
+* @str: the used string reference
+* Return: 0
+*/
+
+void puts2(char *str)
+{ int i = 0;
+
+while (str[i] != '\0')
+{ if (i % 2 == 0)
+{ _putchar(stri[i]); }
+i++; }
+_putchar('\n'); }
