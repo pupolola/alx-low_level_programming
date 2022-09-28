ab005ebfea3c9792259abe89c05ebb27c1908d77
Pupolola <pupololaabdul@gmail.com>
2022-09-28 06:09:54 -0700

    is palindrome

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -31,7 +31,9 @@ return (len); }
 *         If the string is not a palindrome - 0.
 */
 int check_palindrome(char *s, int len, int index)
-{ if (s[index] == s[len / 2])
+
+{
+if (s[index] == s[len / 2])
 return (1);
 
 if (s[index] == s[len - index - 1])
